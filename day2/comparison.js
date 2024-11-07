7279be903d65a312a5844b6d108abf5b585ed6d3
Nitish kumar chaudhary <nitin12345chaudhary@gmail.com>
2024-11-07 16:24:00 +0000

    comp

---
+++ day2/comparison.js
@@ -0,0 +1,15 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null == 0)
+console.log(null > 0)
+
+// imp 
+console.log("Null is greater than 0 :" , null >= 0)
+console.log(null < 0)
+
+console.log(undefined == 0)
+
+// === check not only the value but also the datatype
+
+console.log(2 === "2")
