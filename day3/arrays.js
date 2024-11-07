f127eb038f7d10e69bd5a1462bbc7c99ad6d7c21
Nitish kumar chaudhary <nitin12345chaudhary@gmail.com>
2024-11-07 17:42:22 +0000

    hjj

---
+++ day3/arrays.js
@@ -0,0 +1,14 @@
+// Date method
+
+// let mydate = new Date()
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+// console.log(mydate.getDay());
+// console.log(mydate.getMonth());
+
+// ARRAYS IN JS : can have diff data type values
+const arr = [4 ,5 , 3 , 1 , 2 , true , "Nitish"]
+console.log(arr[0]);
+
+
+
