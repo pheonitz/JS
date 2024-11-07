e1de9905a899f30b4350e64a098d9ce1db9444b0
Nitish kumar chaudhary <nitin12345chaudhary@gmail.com>
2024-11-07 16:17:46 +0000

    op

---
+++ day2/operations.js
@@ -0,0 +1,14 @@
+// imp points
+
+console.log(1 + '2')
+console.log("1" + 2)
+
+// Now
+console.log("1" +2+2)
+console.log(1 + 2+ "2")
+
+// why ??
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
